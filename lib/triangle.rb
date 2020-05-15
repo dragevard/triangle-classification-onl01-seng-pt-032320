9d85e66f1f395af8caa48711e5b315bda7ab0243
Derek phung <dragevards@gmail.com>
2020-05-15 20:48:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,9 +16,13 @@ class Triangle
     else
       if @side1 == @side2 && @side2 == @side2 && @side1 == @side3
         :equilateral 
-      elsif @side1 == @side2 ||  
-        
-      
+      elsif @side1 == @side2 ||  @side1 == @side3 ||  @side3 == @side2
+        :isosceles 
+      else 
+        :scalene
+      end
+    end
+  end
     
   class TriangleError < StandardError 
     "This is not a valid triangle!"
