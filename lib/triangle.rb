3ff1e6cd45342ed6c37adc14bb3492080d50302a
Derek phung <dragevards@gmail.com>
2020-05-15 20:39:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,17 @@
 class Triangle
   
-  def initialize(lengths)
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1 
+    @side2 = side2 
+    @side3 = side3
+  end 
+  
+  def kind 
+    if 
     
+  class TriangeError < StandardError 
+    "This is not a valid triangle!"
+  end
 end
