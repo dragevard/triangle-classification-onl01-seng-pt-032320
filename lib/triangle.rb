ca5fffe669367b93ac84d50f630a05368087c7ae
Derek phung <dragevards@gmail.com>
2020-05-15 20:47:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   
-  attr_accessor :side1, :side2, :side3
+  attr_accessor :side1, :side2, :side3, :equilateral, :isosceles, :scalene
   
   def initialize(side1, side2, side3)
     @side1 = side1 
@@ -14,7 +14,10 @@ class Triangle
     elsif @side1 + @side2 <= @side3 || @side3 + @side2 <= @side1 || @side1 + @side3 <= @side2
       raise TriangleError
     else
-      if @side1 == @side2 &&
+      if @side1 == @side2 && @side2 == @side2 && @side1 == @side3
+        :equilateral 
+      elsif @side1 == @side2 ||  
+        
       
     
   class TriangleError < StandardError 
