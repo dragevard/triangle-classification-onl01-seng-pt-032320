4f3e8192c0da9714e14b6082eea9dadcf6f225fa
Derek phung <dragevards@gmail.com>
2020-05-15 20:45:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,10 @@ class Triangle
   def kind 
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 
       raise TriangleError 
-      elsif @side1 + @side2 <= @side3 || @side3 + @side2 <= @side1 || @side1 + @side3 <= @side2
+    elsif @side1 + @side2 <= @side3 || @side3 + @side2 <= @side1 || @side1 + @side3 <= @side2
       raise TriangleError
     else
+      
       
     
   class TriangleError < StandardError 
